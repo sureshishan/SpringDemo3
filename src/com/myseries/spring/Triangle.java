432d5b68ec94ade7245fa48d46eb6ad6c5f3da30
sureshishan <bethalasuresh@gmail.com>
2017-04-30 11:14:05 +0530

    inner beans

---
+++ src/com/myseries/spring/Triangle.java
@@ -0,0 +1,40 @@
+package com.myseries.spring;
+
+public class Triangle {
+	
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	public void draw()
+	{
+		System.out.println("First point:"+getPointA().getX()+","+getPointA().getY());
+		System.out.println("Second point:"+getPointB().getX()+","+getPointB().getY());
+		System.out.println("Third point:"+getPointC().getX()+","+getPointC().getY());
+	}
+
+}
