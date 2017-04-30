d6d70cb4b0e3e56e2571fc95efc5ac752ef64bc4
sureshishan <bethalasuresh@gmail.com>
2017-04-30 11:19:04 +0530

    added alias name to triangle bean so that we cna use it in get bean or as a reference.

--- src/com/myseries/spring/DrawingApp.java
+++ src/com/myseries/spring/DrawingApp.java
@@ -18,7 +18,7 @@ public class DrawingApp {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		
-		Triangle triangle = (Triangle) context.getBean("triangle");
+		Triangle triangle = (Triangle) context.getBean("triangle-alias");
 		triangle.draw();
 	}
 
